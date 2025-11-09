d11272d504ad3aaec97ec460344592447b1a1e65
Will <zxcv0908905225@gmail.com>
2025-11-10 06:32:06 +0800

    feat: add logging with loguru

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
+
+logger = get_logger()
 
 router = APIRouter(
     prefix="/home",
@@ -7,4 +10,9 @@ router = APIRouter(
 
 @router.get("/")
 def home():
+    logger.info("Home endpoint was called")
+    logger.debug("Debugging home endpoint")
+    logger.warning("This is a warning from the home endpoint")
+    logger.error("This is an error from the home endpoint")
+    logger.critical("This is a critical message from the home endpoint")
     return {"message": "Welcome to the Bank FastAPI application!"}
