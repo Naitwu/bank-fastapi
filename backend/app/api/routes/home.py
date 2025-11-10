c3e18df859d42daf6fcfa1dac28846ca1ce3e1f0
Will <zxcv0908905225@gmail.com>
2025-11-11 03:11:42 +0800

    chore: install celery and email related packages

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -10,9 +10,4 @@ router = APIRouter(
 
 @router.get("/")
 def home():
-    logger.info("Home endpoint was called")
-    logger.debug("Debugging home endpoint")
-    logger.warning("This is a warning from the home endpoint")
-    logger.error("This is an error from the home endpoint")
-    logger.critical("This is a critical message from the home endpoint")
     return {"message": "Welcome to the Bank FastAPI application!"}
